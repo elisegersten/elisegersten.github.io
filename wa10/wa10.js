0efd9da1a113d725adfd041ba9664e4612ea5667
elisegersten <elge2970@colorado.edu>
2024-06-27 11:50:44 -0400

    wa10-Attempt2

--- wa10/wa10.js
+++ wa10/wa10.js
@@ -12,13 +12,13 @@ function randomValueFromArray(array){
 
 //NUMBER2
 
-let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
-let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 
 //NUMBER3
@@ -27,11 +27,11 @@ randomize.addEventListener('click', result);
 
 function result() {
 
-    let newStory= storyText;
+    const newStory= storyText;
 
-    let xItem = randomValueFromArray(insertX);
-    let yItem = randomValueFromArray(insertY);
-    let zItem = randomValueFromArray(insertZ);
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
 
     newStory = newStory.replace(';insertX;', xItem);
     newStory = newStory.replace(';insertY;', yItem);
