91967949e91935082e515174f0bd14065f94a05b
elisegersten <elge2970@colorado.edu>
2024-06-27 12:12:39 -0400

    wa10-Attempt6

--- wa10/wa10.js
+++ wa10/wa10.js
@@ -14,11 +14,11 @@ function randomValueFromArray(array){
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-const inserty = ["the soup kitchen", "Disneyland", "the White House"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
-const insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 
 //NUMBER3
@@ -44,11 +44,11 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(300/14);
+    const temperature =  Math.round((94-32) * 5/9);
 
-    newStory= newStory.replace('300 pounds', weight);
-    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory= newStory.replace('300 pounds', weight + 'stone');
+    newStory = newStory.replace('94 fahrenheit', temperature + 'centigrade');
 
   }
 
