461f8390c649c06dde4b48890b51a0dee90a81e5
elisegersten <elge2970@colorado.edu>
2024-06-27 11:21:34 -0400

    ica10-Attempt2

--- js/ica10.js
+++ js/ica10.js
@@ -1,7 +1,8 @@
 //Change background color
-
+document.getElementById('changecolor').addEventListener('click', function() {document.body.style.backgroundColor = 'blue';});
 
 
 
 
 //Change picture
+document.getElementById('changepicture').addEventListener('click', function() {this.src = 'img/elliecomo2.jpeg';});
