b2941dff98629346c53070928a18a9f38bb93bde
elisegersten <elge2970@colorado.edu>
2024-06-27 12:06:24 -0400

    wa10-Attempt5

--- wa10/wa10.js
+++ wa10/wa10.js
@@ -14,11 +14,11 @@ function randomValueFromArray(array){
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const inserty = ["the soup kitchen", "Disneyland", "the White House"];
 
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
 
 //NUMBER3
