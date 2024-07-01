57632f9f01445d252e7205afb60de6df0c4b9d6e
elisegersten <elge2970@colorado.edu>
2024-07-01 16:56:47 -0400

    jsproject-Attempt3

--- js-project/jsproject.js
+++ js-project/jsproject.js
@@ -100,8 +100,8 @@ while (balls.length < 25) {
     
     random(0 + size, width - size),
     random(0 + size, height - size),
-    random(-3, 3),
-    random(-3, 3),
+    random(-1, 1),
+    random(-1, 1),
     random(0, 9),
     size
   );
