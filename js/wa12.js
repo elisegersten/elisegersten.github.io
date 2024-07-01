3d6454fbba407d8cfc8156498fe72ca060820aba
elisegersten <elge2970@colorado.edu>
2024-06-30 22:30:38 -0400

    wa12-Attempt1

---
+++ js/wa12.js
@@ -0,0 +1,53 @@
+const endpoint = 'https://the-trivia-api.com/v2/questions';
+
+
+const newQuote = document.querySelector('#js-new-quote');
+newQuote.addEventListener('click', getTrivia);
+
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer);
+
+const shareBtn = document.querySelector('#js-share');
+shareBtn.addEventListener('click', shareOnTwitter)
+
+
+let answerTxt = document.querySelector('#js-answer-text');
+let questionTxt = document.querySelector('#js-quote-text');
+let answer = '';
+
+async function getTrivia() {
+    // alert("TEST!"):
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error (response.statusText)
+        }
+        const json = await response.json();
+        //console.log(json)
+        displayTrivia(json['question']);
+        answer = json['answer'];
+        answerTxt.textContent = '';
+    }
+    catch(err) {
+        console. log(err)
+        alert('Falled to fetch new quote');
+    }
+}
+function displayTrivia(quote)  {
+    const triviaText = document.querySelector ('#js-quote-text');
+    triviaText.textContent = quote;
+}
+
+
+function displayAnswer() {
+    answerTxt.textContent = answer;
+}
+
+function shareOnTwitter() {
+    const question = questionTxt.textContent;
+    const answer = answerTxt.textContent;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(question + ' - ' + answer)}`;
+    window.open(twitterURL, '_blank');
+}
+
+
+getTrivia();
