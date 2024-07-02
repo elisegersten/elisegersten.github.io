98d0dab4ec8be2f2e291df91eee1bf14c07828cc
elisegersten <elge2970@colorado.edu>
2024-07-02 12:15:42 -0400

    jsFinal-Attempt9

--- js-project/jsproject.js
+++ js-project/jsproject.js
@@ -13,7 +13,7 @@ const ballsAreaHeight = height;
 let phoneNumberInput = null;
 let submitButton = null;
 
-const fixedSize = 15;
+const fixedSize = 20;
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
